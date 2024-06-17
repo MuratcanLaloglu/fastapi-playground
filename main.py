bb0e1d1cda98b253858501c39dabf072fa9f8a1c
MuratcanLaloglu <quaristr@gmail.com>
2024-06-18 00:08:56 +0300

    added delete post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, Response
 from pydantic import BaseModel
 from random import randrange
 
@@ -22,6 +22,12 @@ def find_post(id: int):
     for p in my_posts:
         if p["id"] == id:
             return p
+
+
+def find_index_post(id: int):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
 
 
 @app.get("/")
@@ -51,3 +57,13 @@ def get_post(id: int):
             detail=f"post with id: {id} not found",
         )
     return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
+    
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
